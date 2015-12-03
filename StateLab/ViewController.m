a79e5b0cf2b70136f39b801869211823e89763fa
oooo0oooo0 <oooo0oooo0@foxmail.com>
2015-12-03 23:16:52 +0800

    进入后台状态后执行任务
    在进入后台和恢复前台时的各种处理

--- StateLab/ViewController.m
+++ StateLab/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 @property(strong,nonatomic) UILabel *label;
+@property(strong,nonatomic) UIImage *smiley;
+@property(strong,nonatomic) UIImageView *smileyView;
+@property(strong,nonatomic) UISegmentedControl *segementControl;
 @end
 
 @implementation ViewController
@@ -21,34 +24,130 @@
     [super viewDidLoad];
     
     CGRect bounds=self.view.bounds;
-    CGRect labelFrame=CGRectMake(bounds.origin.x, CGRectGetMidY(bounds)-50, bounds.size.width, 100);
+    CGRect labelFrame=CGRectMake(bounds.origin.x, CGRectGetMidY(bounds)-25, bounds.size.width, 100);
     self.label=[[UILabel alloc] initWithFrame:labelFrame];
     self.label.font=[UIFont fontWithName:@"Helvetica" size:70];
     self.label.text=@"大风车";
     self.label.textAlignment=NSTextAlignmentCenter;
     self.label.backgroundColor=[UIColor clearColor];
     [self.view addSubview:self.label];
+    
+    CGRect smilyFram=CGRectMake(CGRectGetMaxX(bounds)/2-42, CGRectGetMaxY(bounds)/2-132, 84, 84);
+    self.smileyView=[[UIImageView alloc] initWithFrame:smilyFram];
+    self.smileyView.contentMode=UIViewContentModeCenter;
+    NSString *smileyPath=[[NSBundle mainBundle] pathForResource:@"smiley" ofType:@"png"];
+    self.smiley=[UIImage imageWithContentsOfFile:smileyPath];
+    self.smileyView.image=self.smiley;
+    [self.view addSubview:self.smileyView];
+    
+    self.segementControl=[[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"one",@"two",@"three",@"Four", nil]];
+    self.segementControl.frame=CGRectMake(bounds.origin.x+20, 50, bounds.size.width-40, 30);
+    [self.view addSubview:self.segementControl];
+    
+    //首先尝试读取用户上次点击的分段控件的值
+    //如果读取成功，就恢复为上次点击的值
+    NSNumber *indexNumber=[[NSUserDefaults standardUserDefaults] valueForKey:@"selectedIndex"];
+    if (indexNumber) {
+        NSInteger selectedIndex=[indexNumber intValue];
+        self.segementControl.selectedSegmentIndex=selectedIndex;
+    }
+    
     //开始动画
     //在通知回调中启动动画
     //[self rotateLabelDown];
     
     //接收通知
     NSNotificationCenter *center=[NSNotificationCenter defaultCenter];
-    [center addObserver:self selector:@selector(applicationWillResignActive) name:UIApplicationWillResignActiveNotification object:nil];
-    [center addObserver:self selector:@selector(applicationDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+    
+    //注册不活跃通知
+    [center addObserver:self
+               selector:@selector(applicationWillResignActive)
+                   name:UIApplicationWillResignActiveNotification
+                 object:nil];
+    //注册活跃前通知
+    [center addObserver:self
+               selector:@selector(applicationDidBecomeActive)
+                   name:UIApplicationDidBecomeActiveNotification
+                 object:nil];
+    
+    //注册后台通知
+    [center addObserver:self
+               selector:@selector(applicationDidEnterBackground)
+                   name:UIApplicationDidEnterBackgroundNotification
+                 object:nil];
+    //注册将要进入前台通知
+    [center addObserver:self
+               selector:@selector(applicationWillEnterForground)
+                   name:UIApplicationWillEnterForegroundNotification
+                 object:nil];
 }
 
+//不活跃通知回调
+//设置动画标识为禁止动画
 -(void)applicationWillResignActive
 {
     NSLog(@"VC:%@",NSStringFromSelector(_cmd));
     animate=NO;
 }
-
+//活跃前通知回调
+//设置动画标识为可以开始
 -(void)applicationDidBecomeActive
 {
     NSLog(@"VC:%@",NSStringFromSelector(_cmd));
     animate=YES;
     [self rotateLabelDown];
+}
+
+//后台通知回调
+//释放图片资源
+-(void)applicationDidEnterBackground
+{
+    NSLog(@"VC:%@",NSStringFromSelector(_cmd));
+    
+    UIApplication *app=[UIApplication sharedApplication];
+    
+    
+    __block UIBackgroundTaskIdentifier taskId;
+    //这个地方没太明白
+    taskId=[app beginBackgroundTaskWithExpirationHandler:^{
+        NSLog(@"后台任务超时，并且被终止");
+        [app endBackgroundTask:taskId];
+    }];
+    
+    //申请失败
+    if (taskId==UIBackgroundTaskInvalid) {
+        NSLog(@"申请后台任务失败");
+        return;
+    }
+    //真正进入后台任务
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"进入后台任务,共获得%f秒",app.backgroundTimeRemaining);
+        
+        self.smiley=nil;
+        self.smileyView.image=nil;
+        
+        //进入后台时记录分段控件选择的值
+        //在程序加载时读取
+        NSInteger selectedIndex=self.segementControl.selectedSegmentIndex;
+        [[NSUserDefaults standardUserDefaults] setInteger:selectedIndex forKey:@"selectedIndex"];
+        
+        //模拟10秒任务
+        [NSThread sleepForTimeInterval:10];
+        
+        NSLog(@"后台任务执行完成，剩余%f秒",app.backgroundTimeRemaining);
+        [app endBackgroundTask:taskId];
+    });
+    
+}
+
+//将进入前台回调
+//重新加载图片资源
+-(void)applicationWillEnterForground
+{
+    NSLog(@"VC:%@",NSStringFromSelector(_cmd));
+    NSString *smileyPath=[[NSBundle mainBundle] pathForResource:@"smiley" ofType:@"png"];
+    self.smiley=[UIImage imageWithContentsOfFile:smileyPath];
+    self.smileyView.image=self.smiley;
 }
 
 //文字倒置动画
@@ -75,11 +174,6 @@
                              [self rotateLabelDown];
                          }
                      }];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
